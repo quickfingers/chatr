4c11effde9a59a3afc88bd30c3281c4672c512d3
Efthemios Suyat <bongbox@gmail.com>
2015-05-11 23:17:29 -0400

    logic to create an http server

--- server.js
+++ server.js
@@ -53,3 +53,24 @@ function serveStatic(respponse, cache, absPath)
         });
     }
 }
+
+
+// creating the http server, using anonymous function to define pre-request
+var server = http.createServer(function(request, response){
+    var filePath = false;
+    if(request.url == '/') {
+        // default
+        filePath = 'public/index.html';
+    }else {
+        // url path to relative path
+        filePath = 'public' + request.url;
+    }
+
+    var absPath= "./" + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+
+server.listen(3000, function() {
+    console.log("Server is listening to port 3000.");
+})
